3b563a1edc2bf6811bac92207bd85a1703c203c2
DeliveranceTechSolutions <90012755+DeliveranceTechSolutions@users.noreply.github.com>
2023-12-30 20:08:17 +0000

    graph functionality

--- graph/graph.go
+++ graph/graph.go
@@ -1,12 +1,16 @@
 package graph
 
 type Graph struct {
-	vertices [][]Vertex
+	vertices map[*Vertex]Neighbors // maps are always references
 }
 
 type Vertex struct {
 	degree int32
-	neighbors map[*Vertex][]Vertex
+	neighbors Neighbors
+}
+
+type Neighbors struct {
+	nextDoor map[*Vertex]bool
 }
 
 type Edge struct {
@@ -14,5 +18,25 @@ type Edge struct {
 }
 
 func NewCore() *Graph {
+	return &Graph{}
+}
 
+func (g *Graph) isNeighbor() {
+
+}
+
+func (g Graph) IsCycle() {
+
+}
+
+func (g Graph) IsPlanar() {
+
+}
+
+func (g Graph) IsBiparte() {
+	for vertex, neighbors := range g.vertices {
+		if _, ok := neighbors.nextDoor[vertex]; !ok {
+			
+		}
+	}
 }
