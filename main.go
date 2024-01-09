b646af7982b9467ed7f52efdea992a3413143d6a
DeliveranceTechSolutions <kirklincoln@gmail.com>
2024-01-09 12:53:16 -0800

    going to add channels to graphe

--- main.go
+++ main.go
@@ -10,32 +10,36 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
+	defer wg.Wait()
 	// ctx, cancel := context.WithDeadline(context.Background(), time.Now())
 	// defer cancel()
 	g := graph.NewCore()
+
 	start := time.Now()
+	defer func() {
+		fmt.Println(time.Since(start))
+	}()
+	testConst := int32(100000)
+	testGroups := int(testConst)
 	wg.Add(1)
 	go func() {
-		wg.Add(10000)
-		for i := int32(0); i < 10000; i++ {
+		wg.Add(testGroups)
+		for i := int32(0); i < int32(testConst); i++ {
 			go func(i int32, g graph.IGraph, wg *sync.WaitGroup) {
 				fmt.Println(i)
 				g.Add(i, i, wg, graph.ThankYou, graph.Book)
 			}(i, g, &wg)
 		}
-	wg.Done()
 	}()
+	wg.Done()
+
 	wg.Add(1)
 	go func() {
-		wg.Add(10000)
-		for i := int32(0); i < 10000; i++ {
+		wg.Add(testGroups)
+		for i := int32(0); i < int32(testConst); i++ {
 			go g.Execute(&wg)
 		}
-	wg.Done()
 	}()
-	
-	wg.Wait()
-
-	fmt.Println(time.Since(start))
+	wg.Done()
 	return	
 }
